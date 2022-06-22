5b42f8c6dbda284e2a681cbdf3a69f99982b4032
GerganaAtanasova <gergana.atanasova3333@gmail.com>
2022-06-22 23:49:23 +0300

    initial commit

---
+++ mobilele/src/main/java/bg/softuni/mobilele/web/UserLoginController.java
@@ -0,0 +1,21 @@
+package bg.softuni.mobilele.web;
+
+import bg.softuni.mobilele.model.dto.UserLoginDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserLoginController {
+
+    @GetMapping("/users/login")
+    public String login(){
+
+        return "auth-login";
+    }
+
+    @PostMapping("/user/login")
+    public String login(UserLoginDto userLoginDto){
+        return "redirect:/";
+    }
+}
