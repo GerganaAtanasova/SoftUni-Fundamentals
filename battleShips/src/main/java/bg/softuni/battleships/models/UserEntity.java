a3dc827cbb9f5a4dfb855583480059ab401bdde1
GerganaAtanasova <gergana.atanasova3333@gmail.com>
2022-06-29 20:51:00 +0300

    add base models and resources

---
+++ battleShips/src/main/java/bg/softuni/battleships/models/UserEntity.java
@@ -0,0 +1,70 @@
+package bg.softuni.battleships.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class UserEntity extends BaseEntity {
+
+    @Column(nullable = false,unique = true)
+    private String username;
+
+    @Column(name = "full_name", nullable = false)
+    private String fullName;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false,unique = true)
+    private String email;
+
+    @OneToMany
+    private List<ShipEntity> ships;
+
+    public UserEntity() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<ShipEntity> getShips() {
+        return ships;
+    }
+
+    public void setShips(List<ShipEntity> ships) {
+        this.ships = ships;
+    }
+}
